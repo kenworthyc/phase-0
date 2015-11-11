654f1bfab6535f8fbee45521872a1f51a85395aa
Caroline Kenworthy <kenworthyc@gmail.com>
2015-11-11 11:32:59 -0600

    Add method practice files

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,12 @@
+# Define an Empty Method
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def do_something(tea, cookie, time)
+  puts tea
+  puts cookie
+  puts time
+end
