c8cd9678bea0bbda35cfc943b939cc4d78397d7a
Caroline Kenworthy <kenworthyc@gmail.com>
2015-11-19 14:06:14 -0600

    Add initial solution for split_up

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -20,3 +20,51 @@
 # => =>iii. get 4 names, append to container
 # => => iv. get remainder names, append to container
 # 5. return list of groups to empty container
+
+# 1. Initial Solution
+
+def split_up(list)
+  group_list = []
+  arr = list
+  list_count = arr.length
+
+  if list_count > 5
+    if list_count % 5 == 0 || list_count % 5 == 4 #|| list_count % 5 == 3
+      (list_count / 5 + 1).times do
+      group_list << arr.slice!(0, 5)
+      end
+    elsif
+      list_count % 4 == 0 || list_count % 4 == 3
+      (list_count / 4 + 1).times do
+      group_list << arr.slice!(0, 4)
+      end
+
+    elsif
+      list_count % 5 || list_count % 4 == 2 || 1
+       if list_count < 15 # && list_count % 3 == 0
+          (list_count / 3).times do
+            group_list << arr.slice!(0, 3)
+          end
+       else
+
+          begin
+            group_list << arr.slice!(0, 5)
+            group_list << arr.slice!(0, 4)
+          end until list_count % 5 || list_count % 4 == 0 || 3
+            group_list << arr.slice!(0, 4)
+        end
+    end
+
+  else
+    group_list << list[0, arr.length - 1]
+  end
+  p group_list
+  p list
+end
+
+students = %w{CDK HFD DKL ASA VBM VBM HYC CDK HFD DKL ASA VBM VBM HYC CDK HFD DKL ASA VBM VBM}
+puts students.length
+
+p split_up(students)
+
+# 2. Refactored Solution
