bdae627be53504d9695016fd95bbc9731c46e6d7
Caroline Kenworthy <kenworthyc@gmail.com>
2015-11-24 13:15:48 -0600

    Add solution and reflection

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -65,7 +65,6 @@ end
 
 p number_array
 
-
 # Bonus:
 
 startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
@@ -85,3 +84,27 @@ startup_names.map! do |element|
 end
 
 p startup_names
+
+=begin
+
+What are some general rules you can apply to nested arrays?
+1. If you want to access one element at the deepest level of an array, you need
+*as many index-indicators as there are nested arrays*. On Hole 1, there are four total arrays; to return an element from the deepest array, we needed: array[1][1][2][0].
+2. When iterating through nested arrays, applying a class-specific method to an array will result in a Type error; you have to correctly apply iterators within the first iterator (checking for types with is_a? Array or kind_of? Array).
+
+What are some ways you can iterate over nested arrays?
+Iterating over nested arrays is easy enough to do with an Enumerator; the code
+  just starts to look a little dizzy and repetitive, since in order to access
+  a sub-array you need to set an if-else condition: if the iterated var is
+  an array, set another iterator within the first.
+Since they are simply Enumerators, you can use any number of preset methods
+to iterate: .each, .map, .count, etc...
+
+Did you find any good new methods to implement or did you re-use one you were already familiar with? What was it and why did you decide that was a good option?
+We didn't find any new options that would do the rather messy work of iterating
+ within iterators to access sub-arrays, so in lieu of that, we had to stick
+ with .each. We considered using .flatten to simplify the nested iterators,
+ but that would destroy the array structure, which (since this is a nested array
+ exercise) seemed important.
+
+=end
