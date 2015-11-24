58aac84a944ec036c6e0729112264720256f0c8e
Caroline Kenworthy <kenworthyc@gmail.com>
2015-11-23 23:42:56 -0600

    Add reflection

--- week-6/die-2/my_solution.rb
+++ week-6/die-2/my_solution.rb
@@ -88,4 +88,32 @@ end
 
 
 # Reflection
+=begin
+What were the main differences between this die class and the last one you created
+ in terms of implementation? Did you need to change much logic to get this to work?
+In this die class, the random number had to apply to an array element, not
+directly to a Fixnum. That meant the code needed a little more coaxing to return
+the suitable random element than it had needed to return a random number in a
+range. More or less, I had to move the random number generator range from the
+number of sides to the number of array indices (starting with 0).
 
+What does this exercise teach you about making code that is easily changeable or modifiable?
+Having a template to work from is far simpler than not, certainly. And finding a
+ refactor-friendly method makes altering the code far easier; I was able to test
+ the different effects of .shuffle and .sample quickly after I implemented them.
+
+What new methods did you learn when working on this challenge, if any?
+I learned about .shuffle and I used .sample. In my research, I found it
+interesting that .shuffle takes more time to complete than .sample, since
+.shuffle randomizesthe entire array, whereas .sample plucks one random number
+ from the index.
+
+What concepts about classes were you able to solidify in this challenge?
+The scope of instance variables was ironed out more; I think it strange that
+the argument/s in the initialize constructor aren't automatically instance
+variables, since those are the main inputs that would be modified in Class
+methods. I also clarified the correct use of driver code when testing classes;
+in the first challenge I didn't quite have a feel for the .new constructor,
+and I want to learn a little more about it.
+
+=end
