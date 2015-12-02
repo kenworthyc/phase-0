9aae3cf6601b2b6eb056bba1626d477880e50c91
Caroline Kenworthy <kenworthyc@gmail.com>
2015-12-02 10:52:36 -0600

    Add Chapters 1 and 2

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -5,11 +5,40 @@
 // Program Structure
 // Write your own variable and do something to it.
 
+var ducks = "duck" + "duck" + "duck";
 
-
+var food = prompt("What's your favorite food?");
+alert("Hey, " + food + " is my favorite too!");
 
 // Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
 
+// FizzBuzz
+for (var counter = 0; counter <= 100; counter++) {
+
+  if(counter % 3 == 0 && counter % 5 == 0) {
+    console.log("FizzBuzz");
+  }
+  else if(counter % 5 == 0) {
+    console.log("Buzz");
+  }
+  else if(counter % 3 == 0) {
+    console.log("Fizz");
+  }
+  else {
+    console.log(counter);
+  }
+}
+
+ // Triangle
+var number = 0;
+var string = "";
+while (number <= 7) {
+  number += 1;
+  string += "#";
+    console.log(string);
+}
+
+number = 8
 
 
 // Functions
